ba93c7eacebd4cf88ba4605740c191e5d6e8f24d
Hirokazu Kiyomaru <kiyomaru.tennis927@gmail.com>
2016-10-04 15:12:53 +0900

    Add simple model about moving

---
+++ scripts/Move.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python
+import sys
+
+
+class Move():
+    def __init__(self):
+        pass
+
+    def __call__(self, destination, relation='relative'):
+        # Move to the destination
+        return 0
+
+if __name__ == '__main__':
+    sys.exit(Move()())
